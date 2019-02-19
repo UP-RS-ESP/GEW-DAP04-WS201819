d57a1c38aa78c5d05a2253e740b2ba42450dab8c
Aljoscha Rheinwalt <aljoscha@rheinwalt.berlin>
2019-02-19 10:55:17 +0100

    absolute deviations

--- slope/gaussian_hill_diff_abs.py
+++ slope/gaussian_hill_diff_abs.py
@@ -3,26 +3,31 @@ from matplotlib import pyplot as pl
 
 shape = (7, 10)
 width = 0.1
-xr = np.arange(-0.5, -0.5+shape[1] * width, width)
-yr = np.arange(-1.1, -1.1+shape[0] * width, width)
+xb = np.arange(-0.5, -0.5+(shape[1]+1) * width, width)
+yb = np.arange(-1.1, -1.1+(shape[0]+1) * width, width)
+xc = xb[:-1] + width/2
+yc = yb[:-1] + width/2
+X, Y = np.meshgrid(xc, yc)
+dem = np.exp(-Y*Y-X*X)
+tdx = -2 * X * np.exp(-Y*Y-X*X)
+tdy = -2 * Y * np.exp(-Y*Y-X*X)
 
-dem = np.zeros(shape)
-for i in range(shape[0]):
-    for k in range(shape[1]):
-        dem[i, k] = np.exp(-yr[i]*yr[i]-xr[k]*xr[k])
-
-dx = np.zeros(shape)
-dy = np.zeros(shape)
+dx = np.nan * np.ones(shape)
+dy = np.nan * np.ones(shape)
 dx[:, :-1] = (dem[:, :-1] - dem[:, 1:]) / width
 dy[:-1, :] = (dem[:-1, :] - dem[1:, :]) / width
+dx = np.ma.masked_invalid(dx)
+dy = np.ma.masked_invalid(dy)
 
 fg, ax = pl.subplots(ncols = 2)
-im = ax[0].pcolormesh(xr, yr, dx, cmap = pl.cm.magma_r)
+im = ax[0].pcolormesh(xb, yb, dx-tdx,
+        cmap = pl.cm.seismic, vmin = -1.7, vmax = 1.7)
 cb = fg.colorbar(im, ax = ax[0], orientation = 'horizontal')
-cb.set_label('dx')
-im = ax[1].pcolormesh(xr, yr, dy, cmap = pl.cm.magma_r)
+cb.set_label('absolute dx deviation')
+im = ax[1].pcolormesh(xb, yb, dy-tdy,
+        cmap = pl.cm.seismic, vmin = -1.7, vmax = 1.7)
 cb = fg.colorbar(im, ax = ax[1], orientation = 'horizontal')
-cb.set_label('dy')
+cb.set_label('absolute dy deviation')
 ax[0].set_aspect('equal')
 ax[1].set_aspect('equal')
 pl.show()
