075162fd940229c3b84f8128bbbca2a2a8ff96b6
Aljoscha Rheinwalt <aljoscha@rheinwalt.berlin>
2019-02-25 14:34:06 +0100

    initial griddata

---
+++ interpolate/simplistic_griddata.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.interpolate import griddata
+from matplotlib import pyplot as pl
+
+x = [1, 1, 3, 3]
+y = [1, 2, 1, 2]
+z = [2, 2, 4, 4]
+
+xb = np.arange(0.5, 4, 1)
+yb = np.arange(0.5, 3, 1)
+xc = xb[:-1] + abs(xb[0] - xb[1]) * 0.5
+yc = yb[:-1] + abs(yb[0] - yb[1]) * 0.5
+print(xc, yc)
+
+X, Y = np.meshgrid(xc, yc)
+
+print('X:')
+print(X)
+
+print('Y:')
+print(Y)
+
+Z = griddata((x, y), z, (X, Y))
+
+print('Z:')
+print(Z)
+
+pl.pcolormesh(xb, yb, Z)
+pl.colorbar()
+pl.show()
