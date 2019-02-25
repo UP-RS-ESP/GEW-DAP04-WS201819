ea7418a75f6dad56733feb3c474c0217bc8f3e31
Aljoscha Rheinwalt <aljoscha@rheinwalt.berlin>
2019-02-25 11:21:31 +0100

    save dems

--- binning/pozo_5m_class_dem.py
+++ binning/pozo_5m_class_dem.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 from matplotlib import pyplot as pl
+from rw import WriteGTiff
 
 fn = '../pozo-steep-vegetated-pcl.npy'
 pts = np.load(fn)
@@ -30,19 +31,21 @@ for j in range(len(uc)):
             mean[i, k] = cz[b].mean()
             stdr[i, k] = cz[b].std()
 
+    fname = 'pozo_5m_dem_mean_cl%i.tif' % uc[j]
+    WriteGTiff(fname, mean, x.min(), y.min()+500, step = 5)
+    np.save('pozo_5m_dem_mean_cl%i.npy' % uc[j], mean)
+    np.save('pozo_5m_dem_stdr_cl%i.npy' % uc[j], stdr)
     ax[0, j].set_title('Class %i' % uc[j])
     im = ax[0, j].pcolormesh(xb, yb,
                   np.ma.masked_invalid(mean),
                   cmap = pl.cm.viridis_r)
-    cb = fg.colorbar(im, ax = ax[0, j],
-            orientation = 'horizontal')
+    cb = fg.colorbar(im, ax = ax[0, j])
     cb.set_label('Mean elevation [m]')
 
     im = ax[1, j].pcolormesh(xb, yb,
                   np.ma.masked_invalid(stdr),
                   cmap = pl.cm.magma_r)
-    cb = fg.colorbar(im, ax = ax[1, j],
-            orientation = 'horizontal')
+    cb = fg.colorbar(im, ax = ax[1, j])
     cb.set_label('Elevation STD')
     ax[0, j].set_aspect('equal')
     ax[1, j].set_aspect('equal')
